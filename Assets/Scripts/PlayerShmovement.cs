a656046fad3e527af0da36a528d490f76ed90ab3
Brendon <elderberryjamproductions@outlook.com>
2025-10-13 13:01:50 +0100

    Neatened Code
    Added some regions and some comments.

--- Assets/Scripts/PlayerShmovement.cs
+++ Assets/Scripts/PlayerShmovement.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class PlayerShmovement : MonoBehaviour
 {
+    #region Movement Declarations
     //MOVEMENT SECTION
     public float moveSpeed;
     
-    public Transform orientation;
+    //Not using this right now, will re-enable when I'm doing the rotation and 3rd person camera
+    //public Transform orientation;
 
     private float horizontalInput;
     private float verticalInput;
@@ -15,33 +17,39 @@ public class PlayerShmovement : MonoBehaviour
     private Rigidbody rb;
 
     public float groundDrag;
+    #endregion
 
+    #region Drag Declaratiosn
     //DRAG SECTION
     //and not the one with all the pretty people
     public float playerHeight;
     //We use these masks essentially for important checks! (Touching a wall or floor, etc etc)
     public LayerMask Ground;
     private bool grounded;
+    #endregion
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //freezing rotation so it's not tumbling around nonstop
+        //and grabbing the rigidbody component to an assigned variable so it's easy to access
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         PlayerInput();
-
+        #region Long winded comment rambling
         //checking if we are touching the ground
         //Essentially the Raycast is emmitting from the center of our player (which is 1 unit tall)
         //So the length of the raycast will be half our height, plus a little bit extra, because of physics engine jank,
         //We might not ALWAYS be exactly lined up with the ground for this to trigger.
         //Also gives a small about of leeway with regards to jumps and stuff
+        //Note : i feel like the TF2 dev who couldn't understand why the game needed a png of a coconut to run
+        //this was a nightmare to figure out
+        #endregion
         RaycastHit hit;
         grounded = Physics.Raycast(transform.position, Vector3.down, out hit, 1.1f, Ground);
         Debug.DrawRay(transform.position, Vector3.down * 1.1f, Color.red);
